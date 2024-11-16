4471e9db07cac311dbfdd1c1bc0f2c1ad8f1c20d
Dv-Senna <dvsenna64@gmail.com>
2024-11-16 23:25:34 +0100

    Add alias for better use

--- include/turbolin/turbolin.hpp
+++ include/turbolin/turbolin.hpp
@@ -1,3 +1,13 @@
 #pragma once
 
+#include "turbolin/vector.hpp"
 
+
+namespace turbolin {
+	using Vec2i = turbolin::Vector<int, 2>;
+	using Vec2f = turbolin::Vector<float, 2>;
+	using Vec3i = turbolin::Vector<int, 3>;
+	using Vec3f = turbolin::Vector<float, 3>;
+	using Vec4i = turbolin::Vector<int, 4>;
+	using Vec4f = turbolin::Vector<float, 4>;
+} // namespace turbolin
